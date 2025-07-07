70d4997dae8de4f7781d06718f3ede7fac272606
Maicol Garzón <mgarzonm@unal.edu.co>
2025-07-07 11:02:41 -0500

    First topping route-controller and app initialization

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,2 +1,8 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+
+const toppingRoutes = require("./topping.routes");
+
+router.use("/toppings", toppingRoutes);
+
+module.exports = router;
